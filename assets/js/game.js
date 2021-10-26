972318ecb0211abe20c00811b1380275d9b1b933
Mona Lisa <mekotigi@gmail.com>
2021-10-26 00:43:24 -0400

    add java script data

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,2 +1,33 @@
-alert("hello");
-window.alert("This is an alert! JavaScript is running!");
+// this creat a function name "fight"
+
+
+function fight () 
+{
+    window.alert("The fight has begun!");
+}
+//fight();
+
+var playerName = window.prompt ("what is your Robot's name?");
+//Note the lack of question marks around player Name
+window.alert(playerName);
+
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+var playerName = "Tony the Robot";
+
+// Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
